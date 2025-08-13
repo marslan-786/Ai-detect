7e3b690248343b333aabf17ca4b77114c0914746
marslan-786 <marslansalfias@gmail.com>
2025-08-13 19:26:01 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -17,8 +17,7 @@ channels = [
     {"name": "Black Hat", "link": "https://t.me/+2P-OUmWo1hc0NmNh"},
     {"name": "Impossible", "link": "https://t.me/only_possible_world", "id": "-1002650289632"},
     {"name": "King Zarar", "link": "https://t.me/ZararEra"},
-    {"name": "SYBER", "link": "https://t.me/CRACKEDEVER"},
-    {"name": "Kami", "link": "https://t.me/kami_broken5"}
+    {"name": "SYBER", "link": "https://t.me/CRACKEDEVER"}
 ]
 
 user_states = {}
